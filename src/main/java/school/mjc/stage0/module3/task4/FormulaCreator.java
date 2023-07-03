301be8633802b613c59bcc7536376387f26f215e
Aziz Ulmasov <azizulmasov93@gmail.com>
2023-07-03 13:37:44 +0500

    Task completed

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        double a = 1;
+        double b = 2;
+        double formula = (9 * Math.pow(a, 2) - 5 * b + a - 7) * ((a + b - 4 * a * b) / 2);
+
     }
 }
